5178ae65e5d1b97cc56ec618816b7dcc1c9e156c
cpb100 <cpbailey025@gmail.com>
2020-12-22 18:53:37 +0000

    Added all the relevant pages for the application. Setting up the navigation bar for the bottom of the app, still in progress.

--- app/src/main/java/com/cpb_dev/bluecloudjournal/SettingsPage.java
+++ app/src/main/java/com/cpb_dev/bluecloudjournal/SettingsPage.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Add_Journal extends AppCompatActivity {
+public class SettingsPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add__journal);
+        setContentView(R.layout.activity_settings_page);
     }
 }
