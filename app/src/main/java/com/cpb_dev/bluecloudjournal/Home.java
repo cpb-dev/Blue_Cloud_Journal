e88c65c332c91453ac619a4b9515cfb7929d2fdb
cpb100 <cpbailey025@gmail.com>
2020-12-23 21:21:35 +0000

    Added more navigation functionality, enabling movement between fragments that aren't contained in the toolbar.

--- app/src/main/java/com/cpb_dev/bluecloudjournal/Home.java
+++ app/src/main/java/com/cpb_dev/bluecloudjournal/Home.java
@@ -1,14 +1,35 @@
 package com.cpb_dev.bluecloudjournal;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class Home extends AppCompatActivity {
-
+public class Home extends Fragment {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //Inflating the HomePage fragment
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.activity_home, null);
+
+        Button btnAddJourn = (Button)v.findViewById(R.id.btnAddNewJourn);
+        btnAddJourn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNewJournal();
+            }
+        });
+
+        return v; //Returning the fragment view
+    }
+
+    public void addNewJournal(){
+        //Opening the AddJournal fragment on the page
+        getFragmentManager().beginTransaction().replace(R.id.fragment_container, new AddJournal()).commit();
     }
 }
--- app/src/main/java/com/cpb_dev/bluecloudjournal/NavActivity.java
+++ app/src/main/java/com/cpb_dev/bluecloudjournal/NavActivity.java
@@ -1,8 +1,13 @@
 package com.cpb_dev.bluecloudjournal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NavActivity extends AppCompatActivity {
 
@@ -10,5 +15,36 @@ public class NavActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav);
+
+        BottomNavigationView navBar = findViewById(R.id.nav);
+        navBar.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new Home();
+                    break;
+                case R.id.nav_journals:
+                    selectedFragment = new JournalsPage();
+                    break;
+                case R.id.nav_goals:
+                    selectedFragment = new GoalsPage();
+                    break;
+                case R.id.nav_settings:
+                    selectedFragment = new SettingsPage();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
+        }
+    };
 }
