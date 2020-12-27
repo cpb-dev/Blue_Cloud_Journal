5c0fe1c59febb89e5c4f24653b004ae720511fe7
cpb100 <cpbailey025@gmail.com>
2020-12-27 13:14:10 +0000

    Added the date to the add journal function.

--- app/src/main/java/com/cpb_dev/bluecloudjournal/AddJournal.java
+++ app/src/main/java/com/cpb_dev/bluecloudjournal/AddJournal.java
@@ -2,19 +2,30 @@ package com.cpb_dev.bluecloudjournal;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddJournal extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_add_journal, container, false);
+        //Fragment inflater
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.activity_add_journal, null);
+
+        String cDate = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(new Date());
+        TextView date = (TextView) v.findViewById(R.id.date);
+        date.setText(cDate);
+
+        return v;
     }
 }
